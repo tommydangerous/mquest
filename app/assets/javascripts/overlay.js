2a33833221cbec92d7dc5c730ef01843b7635e46
QuantumVenturess <quantumventuress@gmail.com>
2012-10-18 20:20:11 -0700

    return false for jquery

--- app/assets/javascripts/overlay.js
+++ app/assets/javascripts/overlay.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
 	$('.viewTime').live('click', function() {
 		$('.overlay').show();
 		$('body').addClass('noOverflow');
+		return false;
 	})
 	$('.closeOverlay').live('click', function() {
 		$('.overlay').hide();
 		$('body').removeClass('noOverflow');
+		return false;
 	})
 })
