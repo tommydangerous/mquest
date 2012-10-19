c7b1fa9cb8d27dd56bde62644e8523e1ce19e8f2
QuantumVenturess <quantumventuress@gmail.com>
2012-10-18 20:12:35 -0700

    fixed overlay

--- app/assets/javascripts/overlay.js
+++ app/assets/javascripts/overlay.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 	$('.viewTime').live('click', function() {
 		$('.overlay').show();
+		$('body').addClass('noOverflow');
 	})
 	$('.closeOverlay').live('click', function() {
 		$('.overlay').hide();
+		$('body').removeClass('noOverflow');
 	})
 })
