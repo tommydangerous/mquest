0119fb7c83d61db979ca5a55e8df6c4d071bd0f3
QuantumVenturess <quantumventuress@gmail.com>
2012-11-21 19:44:22 -0800

    commented out config.active_record.default_timezone in application.rb

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,7 @@ module Mquest
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-    config.active_record.default_timezone = :local
+    
+#    config.active_record.default_timezone = :local
   end
 end
